40df18dcd9e77aa0eea0e135fd629c2f387d99d9
Avinash M <59330319+Avinash7714@users.noreply.github.com>
2020-10-04 00:09:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
